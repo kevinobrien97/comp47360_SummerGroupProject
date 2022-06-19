724ddc9573782b4eec1c111b2b225131f1bc2863
vkeane98 <101653196+vkeane98@users.noreply.github.com>
2022-06-19 22:28:44 +0100

    Small changes to support display

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import './App.css';
+import Navbar from "./components/Navbar/navbar.js"
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;

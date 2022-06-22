508cf65eebcd00b13c451b90c5ee7d2a4053cf8a
Kevin O'Brien <kevin.o-brien.5@ucdconnect.ie>
2022-06-22 18:45:32 +0100

    Changed default Django DB to our RDS instance

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+SQLPW = os.environ['SQLPW']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +84,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dublin_bus',
+        'USER': 'shuttleup',
+        'PASSWORD': SQLPW,
+        'HOST': 'shupdublinbus.cimqwuwj7cb7.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 

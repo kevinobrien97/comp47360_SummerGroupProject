bc4c07e644c4e2f03cbe4da1f14811da81e75e7b
Kevin O'Brien <kevin.o-brien.5@ucdconnect.ie>
2022-06-26 23:13:17 +0100

    Added directions functionality, cancel journey functionality

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,6 +1,12 @@
 import "./Map.css";
-import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
-import { useState } from "react";
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+import { useState, useRef } from "react";
 import {
   ButtonGroup,
   Button,
@@ -12,60 +18,101 @@ import {
 import { FaLocationArrow } from "react-icons/fa";
 
 const center = { lat: 53.3473, lng: -6.2591 };
+// not setting libraries directly to prevent a bug
+const libraries = ["places"];
 
 const Map = () => {
-  const [mapLoaded, setMapLoaded] = useState(null) 
+  const [mapLoaded, setMapLoaded] = useState(null);
+  const [directionsOutput, setDirectionsOutput] = useState(null);
+  const [distance, setDistance] = useState("");
+
+  const originRef = useRef("");
+  const destinationRef = useRef("");
+
   // below loads google maps script
   const { isLoaded } = useJsApiLoader({
     // on server must create .env.local file in src to store API key
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
+    libraries,
   });
+
   if (!isLoaded) return console.log("error loading map");
+
+  async function routeCalculator() {
+    console.log('he', originRef.current.value)
+    if (originRef.current.value === "" || destinationRef.current.value === "") {
+      return;
+    }
+    // eslint-disable-next-line no-undef
+    const dirServ = new google.maps.DirectionsService();
+    const results = await dirServ.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.TRANSIT,
+    });
+    setDirectionsOutput(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+  }
+
+  function cancelRoute() {
+    setDirectionsOutput(null);
+    setDistance("");
+    originRef.current.value = "";
+    destinationRef.current.value = "";
+  }
+
   return (
     <div>
-    <Box className="journey-planner">
-      <Grid container direction="row" spacing={2}>
-        <TextField
-          id="origin"
-          placeholder="Origin"
-          variant="outlined"
-        ></TextField>
-        <TextField
-          id="destination"
-          placeholder="Destination"
-          variant="outlined"
-        ></TextField>
-      </Grid>
+      <Box className="journey-planner">
+        <Grid container direction="row" spacing={2}>
+          <Autocomplete>
+            <input
+              id="origin"
+              placeholder="Origin"
+    
+              ref={originRef}
+            />
+          </Autocomplete>
+          <Autocomplete>
+            <input
+              id="destination"
+              placeholder="Destination"
+             
+              ref={destinationRef}
+            />
+          </Autocomplete>
+        </Grid>
 
-      <ButtonGroup>
-        <Button type="submit">Calculate Route</Button>
-        <Button type="submit">Cancel</Button>
-        {/* will need to change this to users location at some stage */}
-        <Button
-            aria-label='center back'
-            
-         size="large"
+        <ButtonGroup>
+          <Button type="submit" onClick={routeCalculator}>Calculate Route</Button>
+          <Button type="submit" onClick={cancelRoute}>Cancel</Button>
+          {/* will need to change this to users location at some stage */}
+          <Button
+            aria-label="center back"
+            size="large"
             onClick={() => mapLoaded.panTo(center)}
-          >{<FaLocationArrow />}</Button>
-      </ButtonGroup>
-     
-    </Box>
-    <div className="google-map">
-      <GoogleMap
-        // to do -- center map on users current location
-        center={center}
-        zoom={15}
-        mapContainerStyle={{ width: "100%", height: "100%" }}
-        // can remove any default controls - should not need these for our app
-        options={{ fullscreenControl: false, streetViewControl: false }}
-        onLoad={(mapLoaded) => setMapLoaded(mapLoaded)}
-      >
+          >
+            {<FaLocationArrow />}
+          </Button>
+        </ButtonGroup>
+      </Box>
+      <div className="google-map">
+        <GoogleMap
+          // to do -- center map on users current location
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          // can remove any default controls - should not need these for our app
+          options={{ fullscreenControl: false, streetViewControl: false }}
+          onLoad={(mapLoaded) => setMapLoaded(mapLoaded)}
+        >
           {/* will need a variant of the below later */}
           {/* <Marker position={center}></Marker> */}
-        {}
-        
-      </GoogleMap>
-    </div>
+          {directionsOutput && <DirectionsRenderer directions={directionsOutput}/>}
+          {}
+        </GoogleMap>
+      </div>
     </div>
   );
 };

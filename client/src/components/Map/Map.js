57affd0a088d768139bcf8319f836ce0d5957236
Kevin O'Brien <kevin.o-brien.5@ucdconnect.ie>
2022-06-26 14:50:03 +0100

    Remove dev map, centre on Dublin

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,13 +1,13 @@
 import "./Map.css";
 import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
 
-const center = { lat: 38.0, lng: -100.0 };
+const center = { lat: 53.3473, lng: -6.2591 };
 
 const Map = () => {
   // below loads google maps script
   const { isLoaded } = useJsApiLoader({
     // on server must create .env.local file in src to store API key
-    googleMapsApiKey: process.env.MAPS_API_KEY,
+    googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
   });
   if (!isLoaded) return console.log("error loading map");
   return (

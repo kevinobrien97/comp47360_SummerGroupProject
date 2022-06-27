91cf58df40851c2d13ada15c27b248b6dc851cd2
vkeane98 <101653196+vkeane98@users.noreply.github.com>
2022-06-27 09:58:00 +0100

    Features Card Outline and Navigation

---
+++ client/src/components/FeaturesCard/MiniNav.js
@@ -0,0 +1,22 @@
+import "./MiniNav.css"
+import "./SideContainer.css"
+import {AppBar, Toolbar, Typography, Stack, Button} from "@mui/material"
+// npm install @mui/material @emotion/react @emotion/styled
+const MiniNav = () => {
+    return (
+        <div className="mininav-items">
+            <AppBar position='static'>
+               <Toolbar>
+                   <Typography variant='h7' fontSize={8} component='div'/>
+                   <Stack direction="row"></Stack> 
+                    <Button color="inherit" fontSize={8}>Nearest Stops</Button>
+                    <Button color="inherit">Route Search</Button>
+                    <Button color="inherit">Stop Search</Button>
+                    <Button color="inherit">Favourites</Button>
+                </Toolbar> 
+            </AppBar>
+        </div>
+    )                 
+
+}
+export default MiniNav
---
+++ client/src/components/FeaturesCard/SideContainer.js
@@ -0,0 +1,12 @@
+import "./SideContainer.css"
+import MiniNav from "./MiniNav.js"
+
+const SideContainer = () => {
+    return (
+        <div className="side-container">
+            <MiniNav />
+        <div className="display"/>
+        </div>
+    )
+}
+export default SideContainer 

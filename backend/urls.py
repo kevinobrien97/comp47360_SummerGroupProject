7a995984a95f8e3885abc6e3751a605e942b6665
Kevin O'Brien <kevin.o-brien.5@ucdconnect.ie>
2022-07-05 12:37:52 +0100

    Fix url import

--- backend/urls.py
+++ backend/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path as url
 from rest_framework import routers
 from dubbus import views 
 from rest_framework_simplejwt.views import (

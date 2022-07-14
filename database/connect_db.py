add455306da7fad8263fbf328ad14445c7bdbae7
Victoria <victoria.keane@ucdconnect.ie>
2022-07-14 13:14:50 +0100

    updated for weather conditions

--- database/connect_db.py
+++ database/connect_db.py
@@ -3,6 +3,8 @@ from sqlalchemy import create_engine
 import os
 SQLPW = os.environ['SQLPW']
 
+engine = create_engine("mysql+mysqlconnector://shuttleup:" + SQLPW + "@shupdublinbus.cimqwuwj7cb7.us-east-1.rds.amazonaws.com:3306/dublin_bus")
+
 connection = engine.connect()
 
 def create_weather_table():

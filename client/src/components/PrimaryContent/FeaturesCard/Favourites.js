c0996af6a3a0ff3b51b722c195abb34f74ff2b7d
Kevin O'Brien <kevin.o-brien.5@ucdconnect.ie>
2022-07-19 18:36:03 +0100

    Fixed useAxios, implemented frontend get/post for favourites

--- client/src/components/PrimaryContent/FeaturesCard/Favourites.js
+++ client/src/components/PrimaryContent/FeaturesCard/Favourites.js
@@ -1,20 +1,68 @@
-import { React, useState, useContext } from "react";
+import { React, useState, useContext, useEffect } from "react";
 import { Autocomplete, TextField, Button } from "@mui/material";
 import classes from "./Favourites.module.css";
 import FavouriteStops from "./FavouriteStops";
 import AuthContext from "../../../context/AuthContext";
 import { Link } from "react-router-dom";
 import Warning from "./Warning";
+import useAxios from "../../../utils/useAxios";
 
 const Favourites = (props) => {
   const { user } = useContext(AuthContext);
   const [error, setError] = useState(null);
+  const [stopsList, setStopsList] = useState([]);
+  // const [selectedStop, setSelectedStop] = useState();
+  const [autocompleteSelection, setAutocompleteSelection] = useState("");
+  const [selectedStopList, setSelectedStopList] = useState(null);
+
+  const [favs, setFavs] = useState("");
+  console.log("exp", user.exp);
+
+  const api = useAxios();
+
+  // function to add a favourite stop to the database for the user
+  const postSample = async (id) => {
+    const response = await api.post("/favourites/", {
+      stop_id: id,
+    });
+    if (response.status === 201) {
+      console.log(response);
+    } else {
+      // change
+      alert("Something went wrong!");
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await api.get("/favourites/");
+      console.log("!!", response.data);
+      setFavs(response.data);
+    } catch {
+      // change
+      setFavs("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    // only want it to run on load - they are being added to the db via postSample above, and also to the stops list
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const busStops = props.stops.map((stop, index) => {
     return { label: stop.stop_name, key: stop.stop_id };
   });
 
-  const [stopsList, setStopsList] = useState([]);
+  const initialStops = (apiRes) => {
+    // check only once
+    console.log('here')
+    for (let i = 0; i < apiRes.length; i++) {
+      console.log(apiRes[i].stop_id);
+    }
+  }
+  initialStops(favs)
+ 
 
   const addStop = (stop) => {
     // remove error initially, reset below on conditional
@@ -33,6 +81,7 @@ const Favourites = (props) => {
         );
 
         if (!inArr) {
+          postSample(stop.key);
           setStopsList((prevStopsList) => {
             return [...prevStopsList, stopObj];
           });
@@ -44,10 +93,6 @@ const Favourites = (props) => {
       }
     }
   };
-
-  // const [selectedStop, setSelectedStop] = useState();
-  const [autocompleteSelection, setAutocompleteSelection] = useState("");
-  const [selectedStopList, setSelectedStopList] = useState(null);
 
   console.log(busStops);
   return (
--- client/src/utils/useAxios.js
+++ client/src/utils/useAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import jwt_decode from "jwt-decode";
-import dayjs from "dayjs";
+// import dayjs from "dayjs";
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 
@@ -11,17 +11,27 @@ const useAxios = () => {
 
   const axiosInstance = axios.create({
     baseURL,
-    headers: { Authorization: `Bearer ${authTokens?.access}` }
+    headers: { Authorization: `Bearer ${authTokens?.access}` },
   });
 
-  axiosInstance.interceptors.request.use(async req => {
+  axiosInstance.interceptors.request.use(async (req) => {
     const user = jwt_decode(authTokens.access);
-    const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+    const expTime = new Date(user.exp * 1000);
+
+    const currentTime = new Date().getTime();
+    // console.log(currentTime)
+
+    // console.log("isexp", new Date(user.exp * 1000));
+
+    const isExpired = expTime - currentTime < 10;
+  //  console.log(isExpired);
+
+  //   const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1; 
 
     if (!isExpired) return req;
 
     const response = await axios.post(`${baseURL}/token/refresh/`, {
-      refresh: authTokens.refresh
+      refresh: authTokens.refresh,
     });
 
     localStorage.setItem("authTokens", JSON.stringify(response.data));

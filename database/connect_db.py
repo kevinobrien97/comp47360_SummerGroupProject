471162bce5cfbf34278edc9d22113063a209ed47
Victoria <victoria.keane@ucdconnect.ie>
2022-07-20 14:59:46 +0100

    weather description as integers

--- database/connect_db.py
+++ database/connect_db.py
@@ -8,7 +8,7 @@ engine = create_engine("mysql+mysqlconnector://shuttleup:" + SQLPW + "@shupdubli
 connection = engine.connect()
 
 def create_weather_table():
-    create_table = f"CREATE TABLE IF NOT EXISTS weather (temperature INTEGER, feels_like INTEGER, time_stamp DATETIME, weather_text VARCHAR(50))"
+    create_table = f"CREATE TABLE IF NOT EXISTS weather (temperature INTEGER, feels_like INTEGER, time_stamp DATETIME, weather_icon INTEGER)"
     try:
         print(connection.execute(create_table).fetchall())
     except Exception as error:
@@ -20,7 +20,7 @@ def insert_weather(value: dict):
         print(connection.execute(empty_table).fetchall())
     except Exception as error: 
         print(error)
-    sql = f"INSERT INTO weather values('{value['temperature']}','{value['feels_like']}','{value['time_stamp']}','{value['weather_text']}')"
+    sql = f"INSERT INTO weather values('{value['temperature']}','{value['feels_like']}','{value['time_stamp']}','{value['weather_icon']}')"
     try:
         print(connection.execute(sql).fetchall())
     except Exception as error:

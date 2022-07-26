4f7c2213687fa5c2750e1b00cf58ffba056a1f41
Kevin O'Brien <kevin.o-brien.5@ucdconnect.ie>
2022-07-26 23:29:41 +0100

    Show stop schedules for each stop in route

--- client/src/components/PrimaryContent/FeaturesCard/Routes/RouteDetails.js
+++ client/src/components/PrimaryContent/FeaturesCard/Routes/RouteDetails.js
@@ -4,6 +4,7 @@ import LoadingSpinner from "../../../LoadingSpinner";
 import { MdStopScreenShare } from "react-icons/md";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Accordion, AccordionSummary } from "@mui/material";
+import StopTable from "../StopTable";
 
 const RouteDetails = (props) => {
   const [loadingSchedule, setLoadingSchedule] = useState(false);
@@ -42,62 +43,62 @@ const RouteDetails = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  //   useEffect(() => {
-  //     const getSchedule = async (route_short_name, trip_headsign, day) => {
-  //       // setError(null);
-  //       setLoadingSchedule(true);
-  //       try {
-  //         console.log("here");
-  //         // fetch returns a promise
-  //         // is asynchronous
-  //         const response = await fetch(
-  //           `http://127.0.0.1:8000/api/fullroutestoptimes/${route_short_name}/${trip_headsign}/${day}/`
-  //         );
-  //         if (!response.ok) {
-  //           // wont continue with next line if error thrown
-  //           throw new Error("Something went wrong loading schedule");
-  //         }
-  //         const routeSchedule = await response.json();
-  //         // filter out any times less than current time
-  //         const filtered = routeSchedule.filter((d) => {
-  //           return d.departure_time >= props.time.toTimeString().split(" ")[0];
-  //         });
-  //         // sort remaining values by time
-  //         const collator = new Intl.Collator(undefined, {
-  //           numeric: true,
-  //           // sensitivity: "base",
-  //         });
-  //         const sorted = filtered.sort((a, b) => {
-  //           return collator.compare(a.departure_time, b.departure_time);
-  //         });
-  //         // different schedule keys have the same stop information sometimes (multiple keys can apply on same day)
-  //         const noDuplicates = sorted.reduce((unique, t) => {
-  //           if (
-  //             !unique.some(
-  //               (trip) =>
-  //                 trip.departure_time === t.departure_time &&
-  //                 trip.stop_headsign === t.stop_headsign
-  //             )
-  //           ) {
-  //             unique.push(t);
-  //           }
-  //           return unique;
-  //         }, []);
-  //         // setSchedule(noDuplicates.splice(0, 10));
-  //         setSchedule(noDuplicates);
-  //       } catch (error) {
-  //         console.log(error.message);
-  //         // setError(error.message);
-  //       }
-  //       setLoadingSchedule(false);
-  //     };
-  //     getSchedule(
-  //       props.route.route_short_name,
-  //       props.route.trip_headsign,
-  //       props.daySelection
-  //     );
-  //     // rerender whenever time is changed
-  //   }, [props.daySelection, props.time]);
+  useEffect(() => {
+    const getSchedule = async (route_short_name, trip_headsign, day) => {
+      // setError(null);
+      setLoadingSchedule(true);
+      try {
+        console.log("here");
+        // fetch returns a promise
+        // is asynchronous
+        const response = await fetch(
+          `http://127.0.0.1:8000/api/fullroutestoptimes/${route_short_name}/${trip_headsign}/${day}/`
+        );
+        if (!response.ok) {
+          // wont continue with next line if error thrown
+          throw new Error("Something went wrong loading schedule");
+        }
+        const routeSchedule = await response.json();
+        // filter out any times less than current time
+        const filtered = routeSchedule.filter((d) => {
+          return d.departure_time >= props.time.toTimeString().split(" ")[0];
+        });
+        // sort remaining values by time
+        const collator = new Intl.Collator(undefined, {
+          numeric: true,
+          // sensitivity: "base",
+        });
+        const sorted = filtered.sort((a, b) => {
+          return collator.compare(a.departure_time, b.departure_time);
+        });
+        // different schedule keys have the same stop information sometimes (multiple keys can apply on same day)
+        const noDuplicates = sorted.reduce((unique, t) => {
+          if (
+            !unique.some(
+              (trip) =>
+                trip.departure_time === t.departure_time &&
+                trip.stop_headsign === t.stop_headsign
+            )
+          ) {
+            unique.push(t);
+          }
+          return unique;
+        }, []);
+        // setSchedule(noDuplicates.splice(0, 10));
+        setSchedule(noDuplicates);
+      } catch (error) {
+        console.log(error.message);
+        // setError(error.message);
+      }
+      setLoadingSchedule(false);
+    };
+    getSchedule(
+      props.route.route_short_name,
+      props.route.trip_headsign,
+      props.daySelection
+    );
+    // rerender whenever time is changed
+  }, [props.daySelection, props.time]);
 
   return (
     <AccordionDetails sx={{ backgroundColor: "whitesmoke" }}>
@@ -108,7 +109,6 @@ const RouteDetails = (props) => {
         <div>
           {routeStops[0] &&
             routeStops.map((stop, index) => (
-              
               <Accordion disableGutters={true}>
                 <AccordionSummary
                   sx={{
@@ -119,7 +119,9 @@ const RouteDetails = (props) => {
                   expandIcon={<ExpandMoreIcon />}
                   aria-controls="panel1a-content"
                   id="panel1a-header"
-                >  {console.log(stop)}
+                >
+                  {" "}
+                  {console.log(stop)}
                   <div
                     style={{
                       marginTop: "auto",
@@ -130,6 +132,15 @@ const RouteDetails = (props) => {
                     {stop.stop_name}
                   </div>
                 </AccordionSummary>
+                <AccordionDetails sx={{ backgroundColor: "whitesmoke" }}>
+                <StopTable
+                  schedule={schedule.filter((item) => {
+                    return item.stop_id === stop.stop_id;
+                  })}   
+                  time={props.time}
+                  daySelection={props.daySelection}
+                ></StopTable>
+                </AccordionDetails>
               </Accordion>
             ))}
         </div>

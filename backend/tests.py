78fa0adf0568d6b9d2d09f96bcdb954d91c3be3b
Kevin O'Brien <kevin.o-brien.5@ucdconnect.ie>
2022-08-05 02:22:30 +0100

    Add unit tests for favourite routes - valid & invalid posts

--- backend/tests.py
+++ backend/tests.py
@@ -11,6 +11,10 @@ sample_users = [
     {"username": "kevinTestDuplicated", "password": "testpassword1234", "password2": "testpassword1234"},
     ]
 
+sample_bus_routes = [
+    {"trip_headsign": "Harristown Bus Garage - Monkstown Avenue (Richmond Grove)", "route_short_name": "4"}
+]
+
 class TestLogin(TestCase):
     def setUp(self):
         for testuser in sample_users:
@@ -111,3 +115,55 @@ class TestRegister(TestCase):
              'password2': sample_users[5]["password2"]}, follow=True)
         result = json.loads(response.content)  
         self.assertTrue('A user with that username already exists.' in result['username']) 
+
+class TestFavouriteRoutes(TestCase):
+    # set up a user
+    def setUp(self):
+        user = User.objects.create(
+            username=sample_users[0]['username']
+            )
+        user.set_password(sample_users[0]['password'])
+        user.save()
+    
+    # set up a method to get the users access token
+    def get_access_token(self):
+        response = self.client.post('/api/token/'
+        , {
+             'username': sample_users[0]["username"],
+             'password': sample_users[0]["password"]}, follow=True)
+        # should be logged in
+        # convert to json format to access the token
+        result = json.loads(response.content)
+        return result["access"]
+
+    def test_post_favourite_route(self):
+        access_token = self.get_access_token()
+        response = self.client.post('/api/favouriteroutes/',
+        {
+            "trip_headsign": sample_bus_routes[0]["trip_headsign"],
+            "route_short_name": sample_bus_routes[0]["route_short_name"]
+        }, 
+        content_type='application/json',
+        HTTP_AUTHORIZATION= f'Bearer {access_token}', 
+        follow=True)
+        self.assertEquals(response.status_code, 201)
+        result = json.loads(response.content)
+        # should be true as its the first favourite added in test database
+        # id is the primary key assigned that also lists the order favourites were added in
+        self.assertEquals(result["id"], 1)
+        self.assertEquals(result["trip_headsign"], "Harristown Bus Garage - Monkstown Avenue (Richmond Grove)")
+        # no need to test entire output
+    
+    def test_post_favourite_route_fail(self):
+
+        response = self.client.post('/api/favouriteroutes/',
+        {
+            "trip_headsign": sample_bus_routes[0]["trip_headsign"],
+            "route_short_name": sample_bus_routes[0]["route_short_name"]
+        }, 
+        content_type='application/json',
+        # HTTP_AUTHORIZATION not provided
+        follow=True)
+        self.assertEquals(response.status_code, 401)
+        result = json.loads(response.content)
+        self.assertEquals(result["detail"], "Authentication credentials were not provided.")

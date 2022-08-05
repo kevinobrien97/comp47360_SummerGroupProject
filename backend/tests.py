ca656bed68b5490bfedeac5588ebbc2bcd9156b7
Kevin O'Brien <kevin.o-brien.5@ucdconnect.ie>
2022-08-05 14:04:20 +0100

    Add unit tests for refresh tokens

--- backend/tests.py
+++ backend/tests.py
@@ -16,106 +16,170 @@ sample_bus_routes = [
     {"trip_headsign": "Dalriada Estate - Merrion Square South", "route_short_name": "15b"}
 ]
 
-# class TestLogin(TestCase):
-#     def setUp(self):
-#         for testuser in sample_users:
-#             user = User.objects.create(
-#                 username=testuser['username']
-#                 )
-#             user.set_password(testuser['password'])
-#             user.save() 
+class TestLogin(TestCase):
+    def setUp(self):
+        # add users to db
+        for testuser in sample_users:
+            user = User.objects.create(
+                username=testuser['username']
+                )
+            user.set_password(testuser['password'])
+            user.save() 
 
-#     def test_login_correct(self):
-#         # send user info to url to see if refresh and access tokens are returned (testing access specifically)
-#         response = self.client.post('/api/token/'
-#         , {
-#              'username': sample_users[0]["username"],
-#              'password': sample_users[0]["password"]}, follow=True)
-#         # should be logged in
-#         # convert to json format to access the tokens
-#         result = json.loads(response.content)
-#         self.assertTrue("access" in result)
+    def test_login_correct(self):
+        # send user info to url to see if refresh and access tokens are returned (testing access specifically)
+        response = self.client.post('/api/token/'
+        , {
+             'username': sample_users[0]["username"],
+             'password': sample_users[0]["password"]}, follow=True)
+        # should be logged in
+        # convert to json format to access the tokens
+        result = json.loads(response.content)
+        self.assertTrue("access" in result)
 
-#     def test_login_incorrect(self):
-#         # send incorrect user info to url to see if an error is returned
-#         response = self.client.post('/api/token/'
-#         , {
-#             'username': sample_users[0]["username"],
-#             #  set the password to the wrong one
-#             'password': sample_users[1]["password"]}, follow=True)
-#         # should not be logged in
-#         # convert to json format to access response
-#         result = json.loads(response.content)    
-#         self.assertTrue("No active account found with the given credentials" in result['detail'])
+    def test_login_incorrect(self):
+        # send incorrect user info to url to see if an error is returned
+        response = self.client.post('/api/token/'
+        , {
+            'username': sample_users[0]["username"],
+            #  set the password to the wrong one
+            'password': sample_users[1]["password"]}, follow=True)
+        # should not be logged in
+        # convert to json format to access response
+        result = json.loads(response.content)    
+        self.assertTrue("No active account found with the given credentials" in result['detail'])
 
-# class TestRegister(TestCase):
-#     # testing if a user can be created
-#     def test_register_correct(self):
-#         response = self.client.post('/api/register/'
-#         , {
-#              'username': sample_users[0]["username"],
-#              'password': sample_users[0]["password"],
-#              'password2': sample_users[0]["password2"]}, follow=True)
-#         result = json.loads(response.content)  
-#         self.assertTrue(sample_users[0]["username"] in result['username']) 
-
-#     # testing if only providing one password is acceptable
-#     def test_register_one_password(self):
-#         response = self.client.post('/api/register/'
-#         , {
-#              'username': sample_users[0]["username"],
-#              'password': sample_users[0]["password"]}, follow=True)
-#         result = json.loads(response.content)  
-#         self.assertTrue('This field is required.' in result['password2']) 
+class TestRefresh(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username=sample_users[0]['username']
+            )
+        user.set_password(sample_users[0]['password'])
+        user.save()
     
-#     # testing giving an incorrect second password
-#     def test_register_wrong_password(self):
-#         response = self.client.post('/api/register/'
-#         , {
-#              'username': sample_users[0]["username"],
-#              'password': sample_users[0]["password"],
-#              # different from the first
-#              'password2': sample_users[1]["password2"]}, follow=True)
-#         result = json.loads(response.content)  
-#         self.assertTrue('Both passwords did not match.' in result['password']) 
+    # test users new access token
+    def test_access_tokens(self):
+        # send user info to url and store access token
+        response_token = self.client.post('/api/token/'
+        , {
+             'username': sample_users[0]["username"],
+             'password': sample_users[0]["password"]}, follow=True)
+        # should be logged in
+        # convert to json format to access the tokens
+        result_token = json.loads(response_token.content)
+        access_token_1 = result_token["access"]
+        # store refresh token
+        refresh_token = result_token["refresh"]
+        response_refresh = self.client.post('/api/token/refresh/'
+        , {
+           'refresh': refresh_token
+        }, follow=True)
 
-#     # testing a weak password
-#     def test_register_short_password(self):
-#         response = self.client.post('/api/register/'
-#         , {
-#              'username': sample_users[3]["username"],
-#              'password': sample_users[3]["password"],
-#              'password2': sample_users[3]["password2"]}, follow=True)
-#         result = json.loads(response.content)  
-#         self.assertTrue('This password is too short. It must contain at least 8 characters.' in result['password']) 
+        # success code
+        self.assertEquals(response_refresh.status_code, 200)
+        result_refresh = json.loads(response_refresh.content)
 
-#     # testing a common password
-#     def test_register_common_password(self):
-#         response = self.client.post('/api/register/'
-#         , {
-#              'username': sample_users[4]["username"],
-#              'password': sample_users[4]["password"],
-#              'password2': sample_users[4]["password2"]}, follow=True)
-#         result = json.loads(response.content)  
-#         self.assertTrue('This password is too common.' in result['password']) 
+        # store new access token
+        access_token_2 = result_refresh["access"]
+        # check that the new access token is different from the first
+        self.assertNotEqual(access_token_1, access_token_2)
+
+    # test users new refresh token
+    # to maintain security a refresh token is valid for a single use and therefore a new one should be returned along with a new access token
+    def test_refresh_tokens(self):
+        # send user info to url and store access token
+        response_token = self.client.post('/api/token/'
+        , {
+             'username': sample_users[0]["username"],
+             'password': sample_users[0]["password"]}, follow=True)
+        # should be logged in
+        # convert to json format to access the tokens
+        result_token = json.loads(response_token.content)
+        access_token = result_token["access"]
+        # store refresh token
+        refresh_token_1 = result_token["refresh"]
+        response_refresh = self.client.post('/api/token/refresh/'
+        , {
+           'refresh': refresh_token_1
+        }, follow=True)
+
+        # success code
+        self.assertEquals(response_refresh.status_code, 200)
+        result_refresh = json.loads(response_refresh.content)
+
+        # store the returned refresh token
+        refresh_token_2 = result_refresh["refresh"]
+        # check returned refresh token is the same as the one that was provided
+        self.assertNotEqual(refresh_token_1, refresh_token_2) 
+
+class TestRegister(TestCase):
+    # testing if a user can be created
+    def test_register_correct(self):
+        response = self.client.post('/api/register/'
+        , {
+             'username': sample_users[0]["username"],
+             'password': sample_users[0]["password"],
+             'password2': sample_users[0]["password2"]}, follow=True)
+        result = json.loads(response.content)  
+        self.assertTrue(sample_users[0]["username"] in result['username']) 
+
+    # testing if only providing one password is acceptable
+    def test_register_one_password(self):
+        response = self.client.post('/api/register/'
+        , {
+             'username': sample_users[0]["username"],
+             'password': sample_users[0]["password"]}, follow=True)
+        result = json.loads(response.content)  
+        self.assertTrue('This field is required.' in result['password2']) 
     
-#     # setUp method to create a user that I will attempt to duplicated below
-#     def setUp(self):
-#         user = User.objects.create(
-#             username=sample_users[5]['username']
-#             )
-#         user.set_password(sample_users[5]['password'])
-#         user.save() 
+    # testing giving an incorrect second password
+    def test_register_wrong_password(self):
+        response = self.client.post('/api/register/'
+        , {
+             'username': sample_users[0]["username"],
+             'password': sample_users[0]["password"],
+             # different from the first
+             'password2': sample_users[1]["password2"]}, follow=True)
+        result = json.loads(response.content)  
+        self.assertTrue('Both passwords did not match.' in result['password']) 
 
-#     # testing a duplicated account (one made successfully in setUp above)
-#     def test_register_duplicated_user(self):
-#         response = self.client.post('/api/register/'
-#         , {
-#              'username': sample_users[5]["username"],
-#              'password': sample_users[5]["password"],
-#              'password2': sample_users[5]["password2"]}, follow=True)
-#         result = json.loads(response.content)  
-#         self.assertTrue('A user with that username already exists.' in result['username']) 
+    # testing a weak password
+    def test_register_short_password(self):
+        response = self.client.post('/api/register/'
+        , {
+             'username': sample_users[3]["username"],
+             'password': sample_users[3]["password"],
+             'password2': sample_users[3]["password2"]}, follow=True)
+        result = json.loads(response.content)  
+        self.assertTrue('This password is too short. It must contain at least 8 characters.' in result['password']) 
+
+    # testing a common password
+    def test_register_common_password(self):
+        response = self.client.post('/api/register/'
+        , {
+             'username': sample_users[4]["username"],
+             'password': sample_users[4]["password"],
+             'password2': sample_users[4]["password2"]}, follow=True)
+        result = json.loads(response.content)  
+        self.assertTrue('This password is too common.' in result['password']) 
+    
+    # setUp method to create a user that I will attempt to duplicated below
+    def setUp(self):
+        user = User.objects.create(
+            username=sample_users[5]['username']
+            )
+        user.set_password(sample_users[5]['password'])
+        user.save() 
+
+    # testing a duplicated account (one made successfully in setUp above)
+    def test_register_duplicated_user(self):
+        response = self.client.post('/api/register/'
+        , {
+             'username': sample_users[5]["username"],
+             'password': sample_users[5]["password"],
+             'password2': sample_users[5]["password2"]}, follow=True)
+        result = json.loads(response.content)  
+        self.assertTrue('A user with that username already exists.' in result['username']) 
 
 class TestFavouriteRoutes(TestCase):
     def setUp(self):
@@ -242,5 +306,5 @@ class TestFavouriteRoutes(TestCase):
         follow=True)
         # success code for deletions
         self.assertEquals(response.status_code, 204)
-        
+
     # note I am not testing posting duplicates or posting wrong information as this is not possible via checks I implemented on the frontend (i.e. no manual entry, user checks, duplicate postings prevented)
